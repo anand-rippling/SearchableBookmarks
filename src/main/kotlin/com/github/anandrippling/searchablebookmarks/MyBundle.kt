39aba4efe94efe92fe646bc9c0e5a8c1f65e4bda
GitHub Action <action@github.com>
2025-03-04 13:38:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/anandrippling/searchablebookmarks/MyBundle.kt
+++ src/main/kotlin/com/github/anandrippling/searchablebookmarks/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.anandrippling.searchablebookmarks
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/anandrippling/searchablebookmarks/services/MyProjectService.kt
+++ src/main/kotlin/com/github/anandrippling/searchablebookmarks/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.anandrippling.searchablebookmarks.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.anandrippling.searchablebookmarks.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
